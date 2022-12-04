ebda5abb67b4b336c9b7ba75511db1b5353b7857
ShayraKelly <108482817+ShayraKelly@users.noreply.github.com>
2022-12-04 20:13:22 -0300

    Update P_indexadm.php

--- TCC2/controller/P_indexadm.php
+++ TCC2/controller/P_indexadm.php
@@ -11,6 +11,8 @@ include "V_verifica-login.php"
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha3844-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <title>Hospital Goiás</title>
     <link rel="shortcut icon" href="../imagens/logo_tittle.png">
+    
+    <link rel="stylesheet" type="text/css" href="../view/cssHiago2.css">
 </head>
 
 <body>
